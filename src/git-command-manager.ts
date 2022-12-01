92f2511bfe1e88af57dcdcf103bc61b6d328e121
Cory Miller <cory-miller@github.com>
2022-12-01 13:53:55 -0500

    format again

--- src/git-command-manager.ts
+++ src/git-command-manager.ts
@@ -96,7 +96,7 @@ class GitCommandManager {
     // Note, this implementation uses "rev-parse --symbolic-full-name" because the output from
     // "branch --list" is more difficult when in a detached HEAD state.
 
-    // TODO(https://github.com/actions/checkout/issues/786): this implementation uses 
+    // TODO(https://github.com/actions/checkout/issues/786): this implementation uses
     // "rev-parse --symbolic-full-name" because there is a bug
     // in Git 2.18 that causes "rev-parse --symbolic" to output symbolic full names. When
     // 2.18 is no longer supported, we can switch back to --symbolic.
